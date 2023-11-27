8bc1b16346ff4f28a24d6f542b6471ecc412cd98
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-27 10:36:10 -0500

    cambios

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ migrate = Migrate(app, db)
 if __name__ == '__main__':
     # app.run(debug=True)  # Comentamos esto, ya que usaremos el servidor de desarrollo de Flask
     db.create_all()
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
