03651ab62b77b2832b165be1a0adaa7773405680
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-27 12:45:53 -0500

    cambios

--- app/models.py
+++ app/models.py
@@ -2,22 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class HistoriaClinica(db.Model):
-    __tablename__ = 'usuarios_historiaclinica'
-    id = db.Column(db.Integer, primary_key=True)
-    diagnosticos = db.Column(db.String(500))
-    tratamientos = db.Column(db.String(500))
-    notas = db.Column(db.String(500))
-    adendas = db.relationship('Adenda', backref='historia_clinica', lazy=True)
-
-class Adenda(db.Model):
-    __tablename__ = 'usuarios_adenda'
-    id = db.Column(db.Integer, primary_key=True)
-    fecha = db.Column(db.String(40))
-    tipo = db.Column(db.String(50))
-    descripcion = db.Column(db.String(500))
-    historia_clinica_id = db.Column(db.Integer, db.ForeignKey('historia_clinica.id'), nullable=False)
-
 class Usuario(db.Model):
     __tablename__ = 'usuarios_usuario'
     documento = db.Column(db.String(15), primary_key=True)
@@ -28,7 +12,3 @@ class Usuario(db.Model):
     edad = db.Column(db.String(30), default='default')
     telefono = db.Column(db.String(30), default='default')
     sexo = db.Column(db.String(30), default='default')
-    historia_clinica_id = db.Column(db.Integer, db.ForeignKey('historia_clinica.id'), unique=True, nullable=True)
-    medico_id = db.Column(db.String(15), db.ForeignKey('usuario.documento'), unique=True, nullable=True)
-    historia_clinica = db.relationship('HistoriaClinica', backref='usuario', lazy=True, uselist=False)
-    medico = db.relationship('Usuario', backref='paciente', remote_side=[documento], uselist=False)
