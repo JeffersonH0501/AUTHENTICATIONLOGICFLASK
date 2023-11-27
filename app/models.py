3d2f5a1f517fb489a55e74455e9c44132db489c1
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-27 10:42:26 -0500

    cambios

--- app/models.py
+++ app/models.py
@@ -7,7 +7,11 @@ class HistoriaClinica(db.Model):
     diagnosticos = db.Column(db.String(500))
     tratamientos = db.Column(db.String(500))
     notas = db.Column(db.String(500))
-    usuario_id = db.Column(db.String(15), db.ForeignKey('usuario.documento'), nullable=True)
+
+medico_paciente = db.Table('medico_paciente',
+    db.Column('medico_documento', db.String(15), db.ForeignKey('usuario.documento')),
+    db.Column('paciente_documento', db.String(15), db.ForeignKey('usuario.documento'))
+)
 
 class Usuario(db.Model):
     documento = db.Column(db.String(15), primary_key=True)
@@ -19,12 +23,13 @@ class Usuario(db.Model):
     sexo = db.Column(db.String(30), default='a')
     foto = db.Column(db.String(255), default='a')
     historia_clinica = db.relationship('HistoriaClinica', backref='usuario', uselist=False, cascade='all, delete-orphan')
+    medico = db.relationship('Usuario', secondary=medico_paciente, backref='pacientes', lazy='dynamic')
 
 class Adenda(db.Model):
     id_adenda = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.String(40))
     tipo = db.Column(db.String(50))
     descripcion = db.Column(db.String(500))
-    historia_clinica_id = db.Column(db.Integer, db.ForeignKey('historia_clinica.id_hc'), nullable=False)
+    historia_clinica = db.Column(db.Integer, db.ForeignKey('historia_clinica.id_hc'), nullable=False)
 
 #db.init_app(app)
