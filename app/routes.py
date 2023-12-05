3310cca7b4ef8bac47cbe9896d5e38512708aa8b
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-12-04 20:52:03 -0500

    cambios

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,17 @@ from flask.views import MethodView
 from sqlalchemy.orm.exc import NoResultFound
 from .models import Usuario
 import jwt
+import hashlib
+
+def hash_dato(dato):
+    hash_object = hashlib.sha256(dato.encode())
+    return hash_object.hexdigest()
 
 def verificar_usuario(documento, clave):
     try:
-        usuario = Usuario.query.filter_by(documento=documento, clave=clave).one()
+        hash_documento = hash_dato(documento)
+        hash_clave = hash_dato(clave)
+        usuario = Usuario.query.filter_by(documento=hash_documento, clave=hash_clave).one()
     except NoResultFound:
         usuario = None
 
